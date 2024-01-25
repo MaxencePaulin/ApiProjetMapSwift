9b342743814071f33ef500c997b95317511439b2
MaxencePaulin <maxence.paulin25@gmail.com>
2024-01-25 15:03:04 +0100

    fix function with populate

--- Services/place-service.js
+++ Services/place-service.js
@@ -18,8 +18,8 @@ exports.getAllPlaces = async () => {
 exports.addUserToPeopleInside = async (idPlace, userId) => {
     try {
         const place = await Place.findById(idPlace).exec();
-        // récupérer les users déjà présents dans peopleInside pour vérifier si je n'existe pas déjà
-        const usersInside = place.peopleInside;
+        // récupérer les users déjà présents dans peopleInside pour vérifier si je n'existe pas déjà [{_id}]
+        const usersInside = place.peopleInside.map(user => user._id);
         // si je n'existe pas déjà dans peopleInside
         if (!usersInside.includes(userId)) {
             // j'ajoute mon id à peopleInside
@@ -40,7 +40,7 @@ exports.addUserToPeopleInside = async (idPlace, userId) => {
 exports.removeUserToPeopleInside = async (idPlace, userId) => {
     try {
         const place = await Place.findById(idPlace).exec();
-        const usersInside = place.peopleInside;
+        const usersInside = place.peopleInside.map(user => user._id);
         if (usersInside.includes(userId)) {
             place.peopleInside.pull(userId);
             const savedPlace = await place.save();
